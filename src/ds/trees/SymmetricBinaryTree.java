7f6ce1bf7048586abaf354012c512b087aaf32a2
senthil20 <senthil.kandasamy20@gmail.com>
2018-05-05 22:29:13 +0530

    update

--- src/ds/trees/SymmetricBinaryTree.java
+++ src/ds/trees/SymmetricBinaryTree.java
@@ -1,41 +1,35 @@
 package ds.trees;
 
 /**
-For example, this binary tree is symmetric:
-       1
-     /   \
-    2     2
-   / \   / \
-  3   4 4   3
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+     1
+    / \
+   2   2
+  / \ / \
+ 3  4 4  3
 
-But the following is not:
-      1
-     / \
-    2   2
-    \   \
-    3    3
- **/
+But the following [1,2,2,null,3,null,3] is not:
+     1
+    / \
+   2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+**/
 
 public class SymmetricBinaryTree {
-
-    public boolean symmetricTree(TreeNode root1, TreeNode root2) {
-
-        if (root1 == null && root2 == null) return true;
-
-        else if (root1 == null || root2 == null) {
-            return false;
-        }
-
-        if(root1.val != root2.val) return false;
-
-        boolean left = symmetricTree(root1.left, root2.right);
-        boolean right = symmetricTree(root1.right, root2.left);
-
-        return (left && right);
+    public boolean symmetricTree(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        boolean isLeft = symmetricTree(left.left, right.right);
+        boolean isRight = symmetricTree(left.right, right.left);
+        return isLeft && isRight;
     }
 
     public static void main(String a[]) {
-
         SymmetricBinaryTree st = new SymmetricBinaryTree();
         TreeNode root = new TreeNode(1,
                 new TreeNode(2,
@@ -44,6 +38,6 @@ public class SymmetricBinaryTree {
                 new TreeNode(2,
                         new TreeNode(4, null, null),
                         new TreeNode(3, null, null)));
-        st.symmetricTree(root.left, root.right);
+        System.out.println(st.symmetricTree(root.left, root.right));
     }
 }
