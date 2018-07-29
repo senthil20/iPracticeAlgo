570ccf355e895fd8c493c3914031507e3209460c
senthil20 <senthil.kandasamy20@gmail.com>
2018-07-29 21:08:04 +0530

    latest check in

---
+++ src/ds/arrays/LongestIncreasingSubsequence.java
@@ -0,0 +1,17 @@
+package ds.arrays;
+
+/**
+ * Given an unsorted array of integers, find the length of longest increasing subsequence.
+ * Example:
+ * Input: [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ */
+
+public class LongestIncreasingSubsequence {
+
+    public static int longestIncreasingSubsequence(int A[]) {
+
+        return 0;
+    }
+}
