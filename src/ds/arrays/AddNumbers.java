cea92ce44f3547943961e2538fc116c677519126
senthil20 <senthil.kandasamy20@gmail.com>
2019-03-13 16:44:34 +0530

    Add numbers

--- src/ds/arrays/AddNumbers.java
+++ src/ds/arrays/AddNumbers.java
@@ -13,7 +13,18 @@ public class AddNumbers {
         return sum;
     }
 
+    public static int addNumbersDP(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i + dp[i - 1];
+        }
+        return dp[n];
+    }
+
     public static void main(String a[]) {
         System.out.println(AddNumbers.addNumbers(6));
+        System.out.println(AddNumbers.addNumbersDP(6));
     }
 }
